19730cc1cec2b9cc56008ed4da506cb310b9fba1
Juanse2527 <159147378+Juanse2527@users.noreply.github.com>
2024-02-13 09:41:44 -0500

    sebas_Clase_3
    Creacion de ciclo de vida

--- app/src/main/java/co/nelson/appuno/MainActivity.java
+++ app/src/main/java/co/nelson/appuno/MainActivity.java
@@ -3,14 +3,16 @@ package co.nelson.appuno;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-   TextView nombreUsuario;
-   Button btn1,btn2;
+    TextView nombreUsuario;
+    Button btn1, btn2;
+    String Tag = "Prueba";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,8 +42,38 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    public double calcularArea(int l1,int l2){
-        return l1*l2;
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i(Tag,"Estoy en OneStart");
     }
 
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i(Tag,"Estoy en OnResume");
+    }
+
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i(Tag,"Estoy en OnPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i(Tag,"Estoy en OnStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i(Tag,"Estoy en OnDestroy");
+    }
 }
+
+
+
